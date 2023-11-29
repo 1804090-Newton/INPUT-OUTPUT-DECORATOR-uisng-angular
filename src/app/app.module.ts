db8c3784cafa5affbc2a02a0415a89fd7622ffe2
Newton <u1804090@student.cuet.ac.bd>
2023-11-29 11:08:37 +0600

    Parent child reationship

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { BlogComponent } from './blog/blog.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BlogComponent,
+    
+
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
