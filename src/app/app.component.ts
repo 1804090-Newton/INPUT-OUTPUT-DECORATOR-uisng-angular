d78907a63051aae18fe45b6d5c216d2be4886b5c
Newton <u1804090@student.cuet.ac.bd>
2023-12-02 09:00:06 +0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,29 @@
-import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, ViewChildren, QueryList } from '@angular/core';
-import { ChildComponent } from './child/child.component';
+import { Component,OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, AfterViewInit {
-  ngAfterViewInit(): void {  // static false hole ei function ti call hobe.
-    this.child.map(x=>console.log(x));
-  }
+export class AppComponent implements OnInit {
+  text!:string; //without initialing "!" this sign must be written.
+ 
+  
+  ischild:boolean=true;
+  ngOnInit():void{// called after dom tree is created. it is used for initialing purphose.
+    console.log("parrent oninit clalled");
 
-  ngOnInit(): void {// static: true hole . ei function ti call hobe.
-    console.log(this.calender);
   }
-  loadCalender()
-     {
-      console.log(this.calender.nativeElement.value)
-        this.input.nativeElement.value=this.calender.nativeElement.value;
+  
+     constructor(){
+      console.log('parent constructor called');
+     
+      
      }
-
-  @ViewChild('calender', { read: ElementRef, static: true }) calender!: ElementRef;// static by default false thake. 
-    // static true mane user view render korar ager state and false mane user view render korar porer state.
-    @ViewChild('input', { read: ElementRef, static: true }) input!: ElementRef;
-    @ViewChildren(ChildComponent)child!:QueryList<ChildComponent>;
-    add()
-      {
-       // this.child.increament();
-      }
+     toggle()//After click operation in html file, toggle() are responsed and it set content of the ischild.
+     {
+      this.ischild=!this.ischild;
+      
+     }
 }
